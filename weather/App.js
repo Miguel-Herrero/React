6f955c1be7110a90176b512561a4ae6e1c5ae479
Miguel-Herrero <spam.maps@icloud.com>
2020-01-19 21:23:20 +0100

    feat: fetch real data from API

--- weather/App.js
+++ weather/App.js
@@ -1,28 +1,71 @@
-import React, { useState } from 'react';
-import { StyleSheet, Text, Platform, KeyboardAvoidingView, ImageBackground, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, Platform, KeyboardAvoidingView, ImageBackground, View, StatusBar, ActivityIndicator } from 'react-native';
+
 import SearchInput from './components/SearchInput'
+
 import getImageForWeather from './utils/getImageForWeather'
+import { fetchLocationId, fetchWeather } from './utils/api'
 
 export default function App() {
-  const [state, setState] = useState('San Francisco')
+  const [isLoading, setIsLoading] = useState(true)
+  const [hasError, setHasError] = useState(false)
+  const [temperature, setTemperature] = useState(0)
+  const [weather, setWeather] = useState('')
+  const [location, setLocation] = useState('San Francisco')
 
-  const handleUpdateLocation = (city) => {
-    setState(city)
+  const handleUpdateLocation = async (city) => {
+    setIsLoading(true)
+    setHasError(false)
+
+    try {
+      const locationId = await fetchLocationId(city)
+      const { location, weather, temperature } = await fetchWeather(locationId)
+
+      setIsLoading(false)
+      setWeather(weather)
+      setTemperature(temperature)
+      setLocation(location)
+    } catch (error) {
+      setIsLoading(false)
+      setHasError(true)
+    }
   }
 
+  useEffect(() => {
+    // Fetch weather when component is first rendered (`setLocation` is fired) and `location` is updated
+    handleUpdateLocation(location)
+  }, [location, setLocation])
+
   return (
-    <KeyboardAvoidingView style={styles.container} behavior="padding">
+    <KeyboardAvoidingView style={styles.container} behavior='padding'>
+      <StatusBar barStyle='light-content' />
       <ImageBackground
         source={getImageForWeather('Light Cloud')}
         style={styles.imageContainer}
         imageStyle={styles.image}>
 
         <View style={styles.detailsContainer}>
-          <Text style={[styles.largeText, styles.textStyle]}>{state}</Text>
-          <Text style={[styles.smallText, styles.textStyle]}>Light Cloud</Text>
-          <Text style={[styles.largeText, styles.textStyle]}>24 ºC</Text>
+          <ActivityIndicator animating={isLoading} color='white' size='large' />
 
-          <SearchInput placeholder="Search any city" onSubmit={handleUpdateLocation} />
+          {!isLoading && (
+            <>
+              {hasError && (
+                <Text style={[styles.smallText, styles.textStyle]}>
+                  Could not load weather, please try a different city.
+                </Text>
+              )}
+
+              {!hasError && (
+                <>
+                  <Text style={[styles.largeText, styles.textStyle]}>{location}</Text>
+                  <Text style={[styles.smallText, styles.textStyle]}>{weather}</Text>
+                  <Text style={[styles.largeText, styles.textStyle]}>{`${Math.round(temperature)} ºC`}</Text>
+                </>
+              )}
+
+              <SearchInput placeholder='Search any city' onSubmit={handleUpdateLocation} />
+            </>
+          )}
         </View>
       </ImageBackground>
     </KeyboardAvoidingView>
