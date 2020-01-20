71a6c9ef6ebf6fc89b3eeda236de2ef8da448b4c
Miguel-Herrero <spam.maps@icloud.com>
2020-01-20 18:31:24 +0100

    feat: (instakilogram) add Activity Indicator for image loading

--- instakilogram/components/Card.js
+++ instakilogram/components/Card.js
@@ -1,10 +1,16 @@
-import React from 'react';
-import { Image, StyleSheet, View } from 'react-native';
+import React, { useState } from 'react';
+import { Image, StyleSheet, View, ActivityIndicator } from 'react-native';
 import PropTypes from 'prop-types';
 
 import AuthorRow from './AuthorRow';
 
 export default function Card({ fullName, image, linkText, onPressLinkText }) {
+  const [isLoading, setIsLoading] = useState(true)
+
+  const handleLoad = () => {
+    setIsLoading(false)
+  }
+
   return (
     <View>
       <AuthorRow
@@ -13,7 +19,13 @@ export default function Card({ fullName, image, linkText, onPressLinkText }) {
         onPressLinkText={onPressLinkText}
       />
 
-      <Image style={styles.image} source={image} />
+      <View style={styles.image}>
+        {isLoading && (
+          <ActivityIndicator style={StyleSheet.absoluteFill} size='large' />
+        )}
+        <Image style={styles.image} source={image} onLoad={handleLoad} />
+
+      </View>
     </View>
   )
 }
