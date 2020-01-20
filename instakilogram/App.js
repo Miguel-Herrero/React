f7bc318ef68703b8697d70d1cd7bd6d2305fcc31
Miguel-Herrero <spam.maps@icloud.com>
2020-01-20 18:00:32 +0100

    feat: (instakilogram) basic author row components

--- instakilogram/App.js
+++ instakilogram/App.js
@@ -1,19 +1,27 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Constants from 'expo-constants';
+
+import AuthorRow from './components/AuthorRow';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <AuthorRow
+        fullName={'John Dow'}
+        linkText={'Comments'}
+        onPressLinkText={() => {
+          console.log('Pressed link!')
+        }}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    marginTop: Constants.statusBarHeight,
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: '#fff'
+  }
 });
---
+++ instakilogram/README.md
@@ -0,0 +1,7 @@
+# Instakilogram
+
+An Instagram-like app with an image feed and comments screen.
+
+## What I learned:
+
+- [Configuring Status Bar](https://docs.expo.io/versions/latest/guides/configuring-statusbar/)
